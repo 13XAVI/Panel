a2672b9abba7331d73b82c055b506df7b8c79822
13XAVI <tresorxavier16@gmail.com>
2023-05-20 11:25:06 +0200

    New Deployment

--- src/main/java/SmartPanel/panel/Reservation/WebConf/WebSecurityConfig.java
+++ src/main/java/SmartPanel/panel/Reservation/WebConf/WebSecurityConfig.java
@@ -43,7 +43,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/HomePage","/AllPanels","/new","/Admin","/edit/**","/Admin/*").authenticated()
+                .antMatchers("/AllPanels","/new","/edit/**","/Admin/*","/AboutUs","/Panels","/panelCost").authenticated()
                 .antMatchers("/new").hasAnyAuthority("ADMIN","DISTRIBUTOR")
                 .antMatchers("/delete/**").hasAnyAuthority("ADMIN")
                 .antMatchers("/new/*").hasAnyAuthority("ADMIN","DISTRIBUTOR")
